4b1f284ce41c405b02a961ee44e0cfcc6971a2cb
Melanie <git@melanie-busse.de>
2025-12-30 21:06:44 +0100

    insert left characters

--- js/createCard.js
+++ js/createCard.js
@@ -26,3 +26,38 @@ form.addEventListener("submit", (event) => {
 form.addEventListener("reset", (event) =>{
    event.target.reset();
 });
+
+document.addEventListener('input', (event) => {
+    const input = event.target.matches(
+        '[data-js="question"], ' +
+        '[data-js="answer"], ' +
+        '[data-js="possibleAnswer1"], ' +
+        '[data-js="possibleAnswer2"], ' +
+        '[data-js="possibleAnswer3"], ' +
+        '[data-js="possibleAnswer4"]')
+        ? event.target
+        : event.target.closest(
+            '[data-js="question"], ' +
+            '[data-js="answer"], ' +
+            '[data-js="possibleAnswer1"], ' +
+            '[data-js="possibleAnswer2"], ' +
+            '[data-js="possibleAnswer3"], ' +
+            '[data-js="possibleAnswer4"]');
+
+    if (!input) return;
+
+    const maxLength = input.maxLength;
+    const currentLength = input.value.length;
+    const charactersLeft = maxLength - currentLength;
+
+    const counter = input.parentElement.querySelector('.characters--left');
+    counter.textContent = `${charactersLeft} characters left`;
+
+    if (charactersLeft <= 10) {
+        counter.style.color = '#dc2626';
+    } else if (charactersLeft <= 30) {
+        counter.style.color = '#eab308';
+    } else {
+        counter.style.color = '#22c55e';
+    }
+});
